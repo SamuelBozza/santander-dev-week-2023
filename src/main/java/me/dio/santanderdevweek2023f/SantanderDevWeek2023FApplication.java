c1ea3b13da07e0e46eb207ef6ab2c6d3c1a28743
Samuel Bozza <102820398+SamuelBozza@users.noreply.github.com>
2023-10-11 09:21:16 -0300

    Initial commit

---
+++ src/main/java/me/dio/santanderdevweek2023f/SantanderDevWeek2023FApplication.java
@@ -0,0 +1,13 @@
+package me.dio.santanderdevweek2023f;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeek2023FApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek2023FApplication.class, args);
+	}
+
+}
